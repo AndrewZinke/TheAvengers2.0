95b1a2110e88feb195e4db110590b70f8e7421e0
jariche1 <jeariche@gmail.com>
2018-08-29 20:22:13 -0400

    Jeremy merged his code

---
+++ WebAPIs/WebAPIStuff/Controllers/WalletsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StockApplication.Models;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+
+namespace StockApplication.Controllers
+{
+	[DataContract(Name = "Wallets")]
+	public class WalletsController : ApiController
+    {
+        private WalletRepository db = new WalletRepository();
+
+       /* // GET: api/Wallets
+        public IQueryable<Wallet> GetWallets()
+        {
+            return db.Wallets.AsQueryable();
+		}
+		*/
+
+        // GET: api/Wallets/5
+		// GET current logged in custoer's wallet
+        [ResponseType(typeof(Wallet))]
+        public IHttpActionResult GetWallet(int id)
+        {
+            Wallet wallet = db.Wallets.Find(id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(wallet);
+        }
+
+		// PUT: api/Wallet/5
+		// PUT: Update the wallet's balance, Active State
+		[ResponseType(typeof(void))]
+        public IHttpActionResult PutWallet(int id, Wallet wallet)
+        {
+			if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+			Debug.WriteLine(wallet.Id);
+            if (id != wallet.Id)
+            {
+                return BadRequest();
+            }
+
+			Debug.WriteLine("Wallet Id Count: " + db.Wallets.Count());
+			Debug.WriteLine("Wallet Id: " + wallet.Id);
+			Debug.WriteLine("Wallet CustomerId: " + wallet.CustomerId);
+			Debug.WriteLine("Wallet Balance: " + wallet.Balance);
+			Debug.WriteLine("Wallet Activite: " + wallet.IsActive);
+
+			db.Entry(wallet).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WalletExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+		// POST: api/Wallets
+		// POST a new wallet if it does not exist for current logged in customer
+		[ResponseType(typeof(Wallet))]
+        public IHttpActionResult PostWallet(Wallet wallet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+			//Manually autoincrement wallet id
+			wallet.Id = db.Wallets.Count() + 1;
+
+			db.Wallets.Add(wallet);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = wallet.Id }, wallet);
+        }
+
+		// POST: api/Wallet
+		// POST a new transaction corresponding to wallet operations for current logged in customer
+		[HttpPost]
+		[Route("api/Transaction")]
+		[ResponseType(typeof(Transaction))]
+		public IHttpActionResult PostWalletTransaction(Transaction transaction)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			Debug.WriteLine("Transaction TransactionId: " + transaction.Id);
+			Debug.WriteLine("Transaction CustomerId: " + transaction.CustomerId);
+			Debug.WriteLine("Transaction WalletId: " + transaction.WalletId);
+			Debug.WriteLine("Transaction Description: " + transaction.Descr);
+			Debug.WriteLine("Transaction Amount: " + transaction.Amount);
+			db.Transactions.Add(transaction);
+			db.SaveChanges();
+
+			return CreatedAtRoute("DefaultApi", new { id = transaction.Id }, transaction);
+		}
+
+		// DELETE: api/Wallets/5
+		/*[ResponseType(typeof(Wallet))]
+        public IHttpActionResult DeleteWallet(int id)
+        {
+            Wallet wallet = db.Wallets.Find(id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            db.Wallets.Remove(wallet);
+            db.SaveChanges();
+
+            return Ok(wallet);
+        }*/
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WalletExists(int id)
+        {
+            return db.Wallets.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- WebAPIs/WebAPIStuff/Models/Wallet.cs
+++ WebAPIs/WebAPIStuff/Models/Wallet.cs
@@ -1,12 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace WebAPIStuff.Models
+namespace StockApplication.Models
 {
-    public class Wallet
-    {
-        public int Id { get; set; }
-    }
+	public class Wallet
+	{
+		//Wallet Id
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		[Key]
+		public int Id { get; set; }
+
+		//Customer Id who owns this wallet 
+		public int? CustomerId { get; set; }
+
+		//Amount of Money available in this wallet
+		public double? Balance { get; set; }
+
+		//State of Wallet {Active Wallet/Inactive Wallet}
+		public bool IsActive { get; set; }
+
+		//Customer details for this wallet
+		//[Required]
+		//NOTE: When I post a new wallet, a new customer is added to the customer table...strange behavior; 
+		//public virtual Customer Customer { get; set; }
+
+		//Transactions that occured for this wallet
+		public virtual List<Transaction> Transactions { get; set; }
+
+		public Wallet()
+		{
+			//Customer = new Customer();
+			Transactions = new List<Transaction>();
+		}
+	}
+
+	//Delete this from wallet Class once we gain access to customer and transaction class from Lance and Andrew
+	public class Customer
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		[Key]
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool IsActive { get; set; }
+		public string Email { get; set; }
+		
+		public virtual Wallet MyWallet { get; set; }
+		public virtual List<Share> Shares { get; set; }
+
+		public Customer()
+		{
+			this.Shares = new List<Share>();
+		}
+	}
+	public class Transaction
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		[Key]
+		public int Id { get; set; }
+		public int? CustomerId { get; set; }
+		public int? WalletId { get; set; }
+		public string Descr { get; set; }
+		public double? Amount { get; set; }
+	}
+	public class Share
+	{
+		[Key]
+		public int Id { get; set; }
+		public int? CustomerId { get; set; }
+		public int? StockId { get; set; }
+		public double? Price { get; set; }
+	}
+	public class Stock
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Symbol { get; set; }
+		public double? Price { get; set; }
+		public int? AmountOfShares { get; set; }
+		public double? Change { get; set; }
+		public double? PercentChange { get; set; }
+		public double? Low { get; set; }
+		public double? High { get; set; }
+	}
 }
